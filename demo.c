d4ea1db2f2dd9172f0155db90b0308b11c3bd8f6
codercheng <chengshuguang@qq.com>
2015-07-11 20:12:32 +0800

    update

--- demo.c
+++ demo.c
@@ -6,13 +6,9 @@
 ev_loop_t * loop = NULL;
 
 void *cb_stdin1(ev_loop_t *loop, int fd, EV_TYPE events);
-void *cb_stdin2(ev_loop_t *loop, int fd, EV_TYPE events);
-void *cb_stdin3(ev_loop_t *loop, int fd, EV_TYPE events);
+void *cb_timer1(ev_loop_t *loop, struct ev_timer *timer);
 
-void *cb_timer1(ev_loop_t *loop, struct ev_timer *timer) {
-    printf("timeout!\n");
-    return NULL;
-}
+
 
 int main() 
 {
@@ -20,26 +16,19 @@ int main()
 
     ev_io_start(loop, 100, 1);
     ev_io_register(loop, 0, EV_READ, cb_stdin1, NULL);
-
+    
     ev_timer_start(loop, 10);
-    ev_timer_register(loop, 5, cb_timer1, 0, NULL);
+    ev_timer_register(loop, 5.0, cb_timer1, 1, NULL);
+
     ev_run_loop(loop);
     return 0;
 }
 
 void *cb_stdin1(ev_loop_t *loop, int fd, EV_TYPE events) {
-    printf("func1: you can read data now\n");
-    ev_io_register(loop, fd, EV_READ, cb_stdin2, NULL);
+    printf("You hava data to read\n");
     return NULL;
 }
-void *cb_stdin2(ev_loop_t *loop, int fd, EV_TYPE events) {
-	printf("func2: you can read data now\n");
-    //ev_io_stop(loop, fd, EV_READ);
-	ev_io_register(loop, fd, EV_READ, cb_stdin3, NULL);
-	return NULL;
-}
-void *cb_stdin3(ev_loop_t *loop, int fd, EV_TYPE events) {
-    printf("func3: you can read data now\n");
-    ev_io_register(loop, fd, EV_READ, cb_stdin1, NULL);
+void *cb_timer1(ev_loop_t *loop, struct ev_timer *timer) {
+    printf("Timeout!\n");
     return NULL;
 }
