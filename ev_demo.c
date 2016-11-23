1a5ab2759a77b0efb91363f79d8b97065f0c80d4
codercheng <chengshuguang@qq.com>
2016-11-23 17:33:44 +0800

    update demo

--- ev_demo.c
+++ ev_demo.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include "ev_loop.h"
 #include "ev_type.h"
-
+#include <time.h>
+#include <stdlib.h>
 
 ev_loop_t * loop = NULL;
 
 void *cb_stdin1(ev_loop_t *loop, int fd, EV_TYPE events);
 void *cb_timer1(ev_loop_t *loop, struct ev_timer *timer);
 
+clock_t b;
 
-
+#define NUM 5000000
 int main() 
 {
     loop = ev_create_loop();
@@ -18,8 +20,14 @@ int main()
     ev_io_register(loop, 0, EV_READ, cb_stdin1, NULL);
     
     ev_timer_init(loop, 10);
-    ev_timer_register(loop, 5.0, cb_timer1, 1, NULL);
-
+    int i;
+    b = clock();
+    for (i=0; i<NUM; i++)
+        ev_timer_register(loop, 0.00000001*(rand()%NUM), cb_timer1, 0, NULL);
+    
+    printf("%f\n", 1.0 * (clock() - b)/CLOCKS_PER_SEC);
+    sleep(10);
+    printf("begin...\n");
     ev_run_loop(loop);
     return 0;
 }
@@ -29,6 +37,13 @@ void *cb_stdin1(ev_loop_t *loop, int fd, EV_TYPE events) {
     return NULL;
 }
 void *cb_timer1(ev_loop_t *loop, struct ev_timer *timer) {
-    printf("Timeout!\n");
+    static int cnt = 0;
+    cnt ++;
+    if (cnt == 1) {
+        b = clock();
+    }
+    if (cnt == NUM) {
+        printf("%f\n", 1.0*(clock() - b)/CLOCKS_PER_SEC);
+    }
     return NULL;
 }
